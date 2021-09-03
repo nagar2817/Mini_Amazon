1359949452e50d729ea3a22b7785776113df96cf
nagar <nagar.2@iitj.ac.in>
2021-09-04 02:23:44 +0530

    adding firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
-  authDomain: "challenge-4b2b2.firebaseapp.com",
-  databaseURL: "https://challenge-4b2b2.firebaseio.com",
-  projectId: "challenge-4b2b2",
-  storageBucket: "challenge-4b2b2.appspot.com",
-  messagingSenderId: "962418448875",
-  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
+  apiKey: "AIzaSyAG8lVFkOZhtOLrkcHES6nYMj0fWk0QJUA",
+  authDomain: "clone-385c3.firebaseapp.com",
+  projectId: "clone-385c3",
+  storageBucket: "clone-385c3.appspot.com",
+  messagingSenderId: "522293263268",
+  appId: "1:522293263268:web:914b0c629ec5af6acd1f31",
+  measurementId: "G-V7Y1WYLSLN"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
